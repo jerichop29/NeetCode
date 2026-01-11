777a2096d64813b13a49e99ee3e56d5bdbbf2a3d
jerichopecho <jerichopecho84@gmail.com>
2026-01-11 17:17:31 +0800

    change something in 283 problem.

--- 283.py
+++ 283.py
@@ -3,16 +3,18 @@ class Solution:
         left = 0
         
         for right in range(len(nums)):
-            # Check if the value of right pointer is zero
+            # Check if the value of right pointer is zero.
             if nums[right] != 0:
                 #Swap the value of left and right pointer
                 nums[left], nums[right] = nums[right], nums[left]
-                #increase the left pointer position
+                #increase the left pointer position.
                 left += 1
+
             else:
                 pass
-        
+                
         return nums
+    
         
 print(Solution.moveZeroes([0,1,0,3,5,1]))
                 
