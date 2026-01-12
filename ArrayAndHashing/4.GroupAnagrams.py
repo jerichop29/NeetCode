a3e82719d8de522ad48e4110ca93905b7b9adee1
jerichopecho <jerichopecho84@gmail.com>
2026-01-12 18:41:02 +0800

    change the approach to solve the problem

--- ArrayAndHashing/4.GroupAnagrams.py
+++ ArrayAndHashing/4.GroupAnagrams.py
@@ -1,17 +1,8 @@
 from collections import Counter
 class Solution:
     def groupAnagrams(strs):
-        list = []
-        left = 0
-        
-        for right in range(1, len(strs)):
-            subList = []
-            if Counter(strs[left]) == Counter(strs[right]):
-                subList.append(strs[right])
-
-            
-            print(subList)
-
+        for key, value in enumerate(strs):
+            pass
 
                 
 print(Solution.groupAnagrams(["opts","tac","tops","cat","stop","pots"]))
